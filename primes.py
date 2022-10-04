2cd87130aad9de3be2b47cb94bb550f91d9725be
muhsin <muhsin.ma.mohamed@gmail.com>
2022-10-05 00:41:50 +0100

    Attempting brute force to find first n primes

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    i = 2
+    while (count < number_of_primes):
+        for j in range(2, i):
+            if (i % j) == 0:
+                break;
+            else:
+                list.append(i)
+                count += 1
+                break;
+        i += 1
     return list
