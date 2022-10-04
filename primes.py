684f73d4d171143b2dc8a9b8607bd72aaff8ffbf
muhsin <muhsin.ma.mohamed@gmail.com>
2022-10-05 00:50:50 +0100

    Correct loop errors

--- primes.py
+++ primes.py
@@ -4,16 +4,18 @@
 def primes(number_of_primes):
     list = []
     count = 0
-    i = 3
+    i = 2
     while (count < number_of_primes):
+        add = True
         for j in range(2, i):
             if (i % j) == 0:
+                add = False
                 break;
-            else:
-                list.append(i)
-                count += 1
-                break;
+        if add:
+            list.append(i)
+            count += 1
+
         i += 1
     return list
 
-print(str(primes(1)))
+print(str(primes(20)))
